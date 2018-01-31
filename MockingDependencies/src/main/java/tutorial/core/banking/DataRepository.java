b560e5495012d8d6d191e510eb7435b9aa3bb0f7
Ehsan Mirsaeedi <mirsaeedi@outlook.com>
2018-01-31 10:28:43 -0500

    Core Banking Business and Tests

--- MockingDependencies/src/main/java/tutorial/core/banking/DataRepository.java
+++ MockingDependencies/src/main/java/tutorial/core/banking/DataRepository.java
@@ -8,6 +8,10 @@ public class DataRepository implements ExternalDependency {
 	public double GetBalanceOfAccount(String accountNumber) {
 	
 		// this method reads the balance amount from the database.
+		// this operation is very slow and resource consuming. 
+		// Using the actual implementation in unit test is not recommended,
+		// because we want our unit tests to run very fast. 
+		// This way we can test our software without losing any extra seconds
 		
 		return 0;
 	}
